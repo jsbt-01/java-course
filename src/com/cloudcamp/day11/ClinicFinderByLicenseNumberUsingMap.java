c1eeddc171eca89b506de16259f53dacf8345e70
Sailendra <lsailendra@castlighthealth.com>
2023-08-01 23:22:06 +0530

    fix
    fix

--- src/com/cloudcamp/day11/ClinicFinderByLicenseNumberUsingMap.java
+++ src/com/cloudcamp/day11/ClinicFinderByLicenseNumberUsingMap.java
@@ -25,17 +25,7 @@ public class ClinicFinderByLicenseNumberUsingMap {
 
     do {
 
-      String reset = "\u001B[0m";
-      String cyan = "\u001B[36m";
-      String yellow = "\u001B[33m";
-      String green = "\u001B[32m";
-
-      System.out.println(cyan + "Menu Options:" + reset);
-      System.out.println(yellow + "  1. " + reset + green + "Enter 1 to add clinic");
-      System.out.println(yellow + "  2. " + reset + green +"Enter 2 to find clinic");
-      System.out.println(yellow + "  3. " + reset + green +"Enter 3 to remove a clinic");
-      System.out.println(yellow + "  4. " + reset + green +"Enter 4 to print all clinics");
-      System.out.println(yellow + "  5. " + reset + green +"Enter 5 to exit");
+      printMenu();
       // Update a clinic by license number
       int userEnteredNumber = scanner.nextInt();
 
@@ -98,6 +88,20 @@ public class ClinicFinderByLicenseNumberUsingMap {
 
     Clinic clinic =  new Clinic(name, pin, license);
     return clinic;
+  }
+
+  public static void printMenu(){
+    String reset = "\u001B[0m";
+    String cyan = "\u001B[36m";
+    String yellow = "\u001B[33m";
+    String green = "\u001B[32m";
+
+    System.out.println(cyan + "Menu Options:" + reset);
+    System.out.println(yellow + "  1. " + reset +  "Enter 1 to add clinic");
+    System.out.println(yellow + "  2. " + reset +  "Enter 2 to find clinic");
+    System.out.println(yellow + "  3. " + reset +  "Enter 3 to remove a clinic");
+    System.out.println(yellow + "  4. " + reset +  "Enter 4 to print all clinics");
+    System.out.println(yellow + "  5. " + reset +  "Enter 5 to exit");
   }
 
 }
